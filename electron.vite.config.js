0766d627096e977655cd1004b989de1300e5e667
awyaki <awyakitaka@gmail.com>
2024-05-01 21:22:55 +0900

    electron-viteを設定

---
+++ electron.vite.config.js
@@ -0,0 +1,18 @@
+import path from "node:path";
+import { defineConfig, externalizeDepsPlugin } from "electron-vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  main: {
+    plugins: [externalizeDepsPlugin()],
+  },
+  preload: {
+    plugins: [externalizeDepsPlugin()],
+  },
+  renderer: {
+    plugins: [react()],
+    resolve: {
+      alias: { "~": path.resolve(__dirname, "src/renderer") },
+    },
+  },
+});
